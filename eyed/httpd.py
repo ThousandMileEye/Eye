0e9e6bc3bb4306db234b1f321934dc315db81920
Naoya Sawada <naoya@tuntunkun.com>
2018-09-13 20:57:22 +0900

    データベーススキーマを自動で更新するように変更 #50 - Alemnic を実行するコードをRPCDから移植

--- eyed/httpd.py
+++ eyed/httpd.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os, subprocess
 from pyramid.config import Configurator
 from waitress import serve
+
+#
+# Database の 最新化
+#
+def init():
+        #
+        # スクリプトを実行するディレクトリ設定
+        #
+        base_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(base_path)
+
+        #
+        # DB を 最新のスキーマ へ アップデート
+        #
+        command = ['alembic upgrade head']
+        subprocess.check_call(command, shell=True)
 
 #
 # デーモンの起動
@@ -13,10 +30,16 @@ def start(host = '0.0.0.0', port = 2018):
 	config = Configurator()
 
 	#
-	# API v1 の 読み込み
+	# API V1 SYSTEM の 読み込み
 	#
-	import api.v1
-	config.include(api.v1.bootstrap, route_prefix='api/v1/')
+	import api.v1.system
+	config.include(api.v1.system.bootstrap, route_prefix='api/v1/system/')
+
+	#
+	# API V1 SERVICE の 読み込み
+	#
+	import api.v1.service
+	config.include(api.v1.service.bootstrap, route_prefix='api/v1/service/')
 
 	#
 	# HTTPDサーバの設定
@@ -28,5 +51,6 @@ def start(host = '0.0.0.0', port = 2018):
 # Main
 #
 if __name__ == '__main__':
+	init()
 	start()
 
