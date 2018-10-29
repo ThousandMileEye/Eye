2c098ebcef6441364c2b225d8f31f8ace213a6dd
Naoya Sawada <naoya@tuntunkun.com>
2018-10-29 23:41:16 +0900

    テストケースの修正 - URLの変更

--- test_cases/api_v1_system.py
+++ test_cases/api_v1_system.py
@@ -10,7 +10,7 @@ url = 'http://127.0.0.1:2018'
 
 class System(unittest.TestCase):
 	def test_getVersion(self):
-		response = requests.get('%s/api/v1/system/version' %(url))
+		response = requests.get('%s/api/v1/system/version/' %(url))
 		self.assertEqual(response.status_code, 200)
 
 	def test_getNetworkInterfaces(self):
