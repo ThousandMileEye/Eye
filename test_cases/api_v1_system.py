1cc1a4c593139e12d3c298c825bd05b691054aed
Naoya Sawada <naoya@tuntunkun.com>
2018-11-02 21:35:55 +0900

    test case check

--- test_cases/api_v1_system.py
+++ test_cases/api_v1_system.py
@@ -9,11 +9,12 @@ import requests
 url = 'http://127.0.0.1:2018'
 
 class System(unittest.TestCase):
-	def test_getVersion(self):
+	def test_get_version(self):
 		response = requests.get('%s/api/v1/system/version/' %(url))
 		self.assertEqual(response.status_code, 200)
 
-	def test_getNetworkInterfaces(self):
+	def test_get_network_interfaces(self):
 		response = requests.get('%s/api/v1/system/network_interfaces/' %(url))
+		print response.content
 		self.assertEqual(response.status_code, 200)
 
