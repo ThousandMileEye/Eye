4ca67a1d550e7b842be0c01bac73f41f233e45c3
Naoya Sawada <naoya@tuntunkun.com>
2019-01-20 11:59:26 +0900

    コンフリクト修正

--- eyed/api/common/cors.py
+++ eyed/api/common/cors.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 from pyramid.request import Request
 from pyramid.response import Response
-=======
-<<<<<<< HEAD
-from pyramid.request import Request
-from pyramid.response import Response
-=======
->>>>>>> fc0f4f6487538e5a7fc9a46c91bac71d642e3f0c
->>>>>>> 5b1abcfd708bf008c6a968d8e4872d16800c2d38
 
 def request_factory(environ):
 	request = Request(environ)
